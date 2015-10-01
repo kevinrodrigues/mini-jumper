f2e7554f8b06263cb305f0462cf3ad3539760860
Kevin Rodrigues <kevin.rodrigues@digitaslbi.com>
2015-10-01 16:40:45 +0100

    Added assets to project.

--- js/preload.js
+++ js/preload.js
@@ -3,10 +3,11 @@ var Preload = function(game){};
 Preload.prototype = {
 
 	preload: function(){ 
-
+		this.game.load.image('title', 'assets/ground_grass_broken.png');
+		this.game.load.image('player', 'assets/bunny1_stand.png');
 	},
 
 	create: function(){
-		this.game.state.start("GameTitle");
+		this.game.state.start("Main");
 	}
 }
