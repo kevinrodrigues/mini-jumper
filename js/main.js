ffbc16fcac8fa72a9c6fe0251fc9d5ad444fdd8b
Kevin Rodrigues <kevin.rodrigues@digitaslbi.com>
2015-10-07 15:46:21 +0100

    Added tiles function.

--- js/main.js
+++ js/main.js
@@ -5,7 +5,45 @@ var Main = function(game){
 Main.prototype = {
 
 	create: function() {
+		var me = this;
 
+		//get floor dimentions..
+		me.titleWidth = me.game.cache.getImage('tile').width;
+		me.tileHeight = me.game.cache.getImage('tile').height;
+
+		//bg color..
+		me.game.stage.backgroundColor = '479cde';
+
+		// enable arcade physics system..
+		me.game.physics.startSystem(Phaser.Physics.ARCADE);
+
+		//add platforms group..
+		me.platforms = me.game.add.group();
+		me.platforms.enableBody = true;
+		me.platforms.createMultiple(250, 'tile');
+	},
+
+	addTile: function(x, y) {
+
+		var me = this,
+			//get tile not on screen..
+			tile = me.platforms.getFirstDead();
+
+		tile.reset(x, y);
+		tile.body.velocity.y = 150;
+		tile.body.immovable = true;
+
+		//remove tile when off screen..
+		tile.checkWorldBounds = true;
+		tile.outOfBoundsKill = true;
+
+	},
+
+	addPlatform: function(y) {
+
+		var me = this;
+
+		
 	},
 
 	update: function() {
--- js/preload.js
+++ js/preload.js
@@ -3,7 +3,7 @@ var Preload = function(game){};
 Preload.prototype = {
 
 	preload: function(){ 
-		this.game.load.image('title', 'assets/ground_grass_broken.png');
+		this.game.load.image('floor', 'assets/ground_grass_broken.png');
 		this.game.load.image('player', 'assets/bunny1_stand.png');
 	},
 
